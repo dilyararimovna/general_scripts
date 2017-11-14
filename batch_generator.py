636f5f6b74c0e4c94b0f0d3e37ffb2a69f72abce
dilyararimovna <dilyara.rimovna@gmail.com>
2017-11-14 18:09:52 +0300

    fixe: fixed batch generator

--- batch_generator.py
+++ batch_generator.py
@@ -3,8 +3,9 @@ from fasttext_embeddings import text2embeddings
 
 class BatchIteratorFromFasttext(object):
 
-    def __init__(self, data, batch_size, text_size, embedding_size, embedding_model, shuffle=False):
+    def __init__(self, data, labels, batch_size, text_size, embedding_size, embedding_model, shuffle=False):
         self._data = data
+        self._labels = labels
         self._num_samples = len(data)
         self._batch_size = batch_size
         self._text_size = text_size
@@ -12,6 +13,7 @@ class BatchIteratorFromFasttext(object):
         self._embedding_model = embedding_model
         self._shuffle = shuffle
         self._current_permutation = None
+        self._epochs_done = 0
 
         self._cursor = 0
         if self._shuffle:
@@ -22,11 +24,19 @@ class BatchIteratorFromFasttext(object):
     def next_batch(self):
         batch = np.zeros(shape=(self._batch_size, self._embedding_size), dtype=np.float)
         if self._cursor + self._batch_size >= self._num_samples:
-            self._cursor = self._cursor % self._num_samples
+            text_batch = self._data[self._current_permutation[self._cursor:]]
+            batch = text2embeddings(text_batch, self._embedding_model, self._text_size, self._embedding_size)
+            batch_labels = self._labels[self._current_permutation[self._cursor:]]
 
-        text_batch = self._current_permutation[self._cursor:self._cursor+self._batch_size]
-        self._cursor += self._batch_size
+            self._cursor = 0
+            self._epochs_done += 1
+            print("___%d epochs done___" % self._epochs_done)
+        else:
+            text_batch = self._data[self._current_permutation[self._cursor:self._cursor+self._batch_size]]
 
-        batch = text2embeddings(text_batch, self._embedding_model, self._text_size, self._embedding_size)
-        return batch
+            batch = text2embeddings(text_batch, self._embedding_model, self._text_size, self._embedding_size)
+            batch_labels = self._labels[self._current_permutation[self._cursor:self._cursor+self._batch_size]]
 
+            self._cursor += self._batch_size
+
+        return batch, batch_labels
